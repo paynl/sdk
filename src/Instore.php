f5ed5ed03f0fee535c5279402c9747813323ea0b
ivodvb <ivo@dewebdivisie.nl>
2015-12-16 11:52:27 +0100

    Reformatted code

--- src/Instore.php
+++ src/Instore.php
@@ -18,9 +18,9 @@
 
 namespace Paynl;
 
+use Paynl\Api\Instore as Api;
 use Paynl\Result\Instore as Result;
 
-use Paynl\Api\Instore as Api;
 /**
  * Description of Transaction
  *
@@ -28,29 +28,31 @@ use Paynl\Api\Instore as Api;
  */
 class Instore
 {
-    public static function status($options){
-        $api = new Api\Status();
+	public static function status($options)
+	{
+		$api = new Api\Status();
 
-        if(isset($options['hash'])){
-            $api->setHash($options['hash']);
-        }
-       
-        $result = $api->doRequest();
+		if (isset($options['hash'])) {
+			$api->setHash($options['hash']);
+		}
 
-        return new Result\Status($result);
-    }
+		$result = $api->doRequest();
 
-    public static function getAllTerminals(){
-        $api = new Api\GetAllTerminals();
+		return new Result\Status($result);
+	}
 
-        if(isset($options['hash'])){
-            $api->setHash($options['hash']);
-        }
+	public static function getAllTerminals()
+	{
+		$api = new Api\GetAllTerminals();
 
-        $result = $api->doRequest();
+		if (isset($options['hash'])) {
+			$api->setHash($options['hash']);
+		}
 
-        return new Result\Status($result);
-    }
+		$result = $api->doRequest();
+
+		return new Result\Status($result);
+	}
 
 	/**
 	 * Send the payment to a terminal
@@ -59,27 +61,18 @@ class Instore
 	 *
 	 * @return Result\Payment
 	 */
-    public static function payment($options){
-        $api = new Api\Payment();
+	public static function payment($options)
+	{
+		$api = new Api\Payment();
 
-        if(isset($options['transactionId'])){
-            $api->setTransactionId($options['transactionId']);
-        }
-        if(isset($options['terminalId'])){
-            $api->setTerminalId($options['terminalId']);
-        }
+		if (isset($options['transactionId'])) {
+			$api->setTransactionId($options['transactionId']);
+		}
+		if (isset($options['terminalId'])) {
+			$api->setTerminalId($options['terminalId']);
+		}
 
-        $result = $api->doRequest();
-        return new Result\Payment($result);
-    }
-
-    
-
-<<<<<<< HEAD
 		$result = $api->doRequest();
 		return new Result\Payment($result);
 	}
 }
-=======
-}
->>>>>>> parent of 4c032ff... Removed empty lines

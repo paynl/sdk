c5b6daddc6df3295985350ad4d529e48f70668e0
Andy Pieters <andy@pay.nl>
2017-01-17 17:46:56 +0100

    added function Service\getPaylinkUrl

---
+++ src/Service.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Copyright (C) 2015 Andy Pieters <andy@andypieters.nl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Paynl;
+use Paynl\Api\Service\GetPayLinkUrl;
+
+
+/**
+ * Description of Paymentmethods
+ *
+ * @author Andy Pieters <andy@andypieters.nl>
+ */
+class Service
+{
+    public static function getPayLinkUrl($options = [])
+    {
+        $api = new GetPayLinkUrl();
+
+        if (!isset($options['securityMode'])){
+           throw new Error\Required('securityMode');
+        }
+        $api->setSecurityMode($options['securityMode']);
+
+        if(!isset($options['amount'])){
+            throw new Error\Required('amount');
+        }
+        $api->setAmount(round($options['amount']*100));
+
+        if(!isset($options['amountMin'])){
+            throw new Error\Required('amountMin');
+        }
+        $api->setAmountMin(round($options['amountMin']*100));
+
+        if(isset($options['countryCode'])){
+            $api->setCountryCode($options['countryCode']);
+        }
+        if(isset($options['language'])){
+            $api->setLanguage($options['language']);
+        }
+        if(isset($options['extra1'])){
+            $api->setExtra1($options['extra1']);
+        }
+        if(isset($options['extra2'])){
+            $api->setExtra2($options['extra2']);
+        }
+        if(isset($options['extra3'])){
+            $api->setExtra3($options['extra3']);
+        }
+        if(isset($options['tool'])){
+            $api->setTool($options['tool']);
+        }
+        if(isset($options['info'])){
+            $api->setInfo($options['info']);
+        }
+        $result = $api->doRequest();
+        return $result['url'];
+    }
+}

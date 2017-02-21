2c27741a085f076b2e277e65192ce6067ca8bdc9
Andy Pieters <andy@pay.nl>
2017-02-21 16:59:19 +0100

    Moved config.php to config.sample.php

---
+++ samples/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+\Paynl\Config::setApiToken('bf81d83f6f8ca32bca272dfdcaf2f14902ff41');
+\Paynl\Config::setServiceId('SL-3490-4320');
+
+//optional: you can download it on https://curl.haxx.se/ca/cacert.pem
+//\Paynl\Config::setCAInfoLocation('path/to/cacert.pem');
+// Or you can skip verifyPeer
+//\Paynl\Config::setVerifyPeer(false);

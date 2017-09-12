10d20b1e94d02a874a5db4b9dfc5a5903bd14917
Andy Pieters <andy@pay.nl>
2017-09-12 13:09:54 +0200

    Added example for minimum required data to start a transaction

---
+++ samples/transaction/start-minimum.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Copyright (C) 2015 Andy Pieters <andy@pay.nl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once '../../vendor/autoload.php';
+require_once '../config.php';
+
+try {
+    $result = \Paynl\Transaction::start(array(
+        'amount' => 12.5,
+        'returnUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/return.php',
+        'ipaddress' => \Paynl\Helper::getIp(),
+    ));
+
+	// Save this transactionId and link it to your order
+    $transactionId = $result->getTransactionId();
+
+    // redirect user to payment page
+    header('location: '.$result->getRedirectUrl());
+
+} catch (\Paynl\Error\Error $e) {
+	// An error has occurred
+    echo "Fout: " . $e->getMessage();
+}

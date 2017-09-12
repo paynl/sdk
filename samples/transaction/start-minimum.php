84c562d48ca33f896107e3ca93c4b60052b770c3
Andy Pieters <andy@pay.nl>
2017-09-12 13:49:16 +0200

    Updated start-minimum.php

--- samples/transaction/start-minimum.php
+++ samples/transaction/start-minimum.php
@@ -21,9 +21,15 @@ require_once '../config.php';
 
 try {
     $result = \Paynl\Transaction::start(array(
+    	// Required
         'amount' => 12.5,
         'returnUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/return.php',
         'ipaddress' => \Paynl\Helper::getIp(),
+
+	    // Optional
+        'exchangeUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/exchange.php',
+        'paymentMethod' => 10,// iDEAL use \Paynl\PaymentMethods::getList() to get all available paymentmethods
+        'description' => '123456', // the transaction description, usually the orderId
     ));
 
 	// Save this transactionId and link it to your order

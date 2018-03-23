7d1c409e39ccf3e9058e31a83051a500489dd841
Andy Pieters <andy@pay.nl>
2018-03-23 13:47:02 +0100

    The uuid should be in hex. Converted the reference to hex, reference can only be 8 chars now

--- samples/DynamicUUID/decode.php
+++ samples/DynamicUUID/decode.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../../vendor/autoload.php";
 
-$uuid   = 'sl49b880-1234-1234-inv2-018000178532';
+$uuid   = 'b0898a33-1234-1234-0000-494e56303031';
 $secret = 'abcdef1234567890abcdef1234567890abcdef12';
 
 try {

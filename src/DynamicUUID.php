e102b9fac8391a15cccd2c1b72961fba23ef8181
andy <andy@pay.nl>
2019-01-18 12:27:08 +0100

    Added links and base64 of ideal and bancontact qrcodes

--- src/DynamicUUID.php
+++ src/DynamicUUID.php
@@ -21,11 +21,11 @@ class DynamicUUID
      *
      * @return string The UUID
      */
-    public static function encode($serviceId, $secret, $reference, $padChar = '0', $referenceType=self::REFERENCE_TYPE_STRING)
+    public static function encode($serviceId, $secret, $reference, $padChar = '0', $referenceType = self::REFERENCE_TYPE_STRING)
     {
         if ($referenceType == self::REFERENCE_TYPE_STRING) {
             self::validateReferenceString($reference);
-        } else if($referenceType == self::REFERENCE_TYPE_HEX) {
+        } else if ($referenceType == self::REFERENCE_TYPE_HEX) {
             self::validateReferenceHex($reference);
         }
 
@@ -52,38 +52,77 @@ class DynamicUUID
 
     private static function validateSecret($strSecret)
     {
-        if (! preg_match('/^[0-9a-f]{40}$/i', $strSecret)) {
+        if (!preg_match('/^[0-9a-f]{40}$/i', $strSecret)) {
             throw new Error('Invalid secret');
         }
     }
 
     private static function validateServiceId($strServiceId)
     {
-        if (! preg_match('/^SL-[0-9]{4}-[0-9]{4}$/', $strServiceId)) {
+        if (!preg_match('/^SL-[0-9]{4}-[0-9]{4}$/', $strServiceId)) {
             throw new Error('Invalid service ID');
         }
     }
 
     private static function validateReferenceString($strReference)
     {
-        if ( ! preg_match('/^[0-9a-zA-Z]{0,8}$/i', $strReference)) {
+        if (!preg_match('/^[0-9a-zA-Z]{0,8}$/i', $strReference)) {
             throw new Error('Invalid reference: only alphanumeric chars are allowed, up to 8 chars long');
         }
     }
 
     private static function validateReferenceHex($strReference)
     {
-        if ( ! preg_match('/^[0-9a-f]{0,16}$/i', $strReference)) {
+        if (!preg_match('/^[0-9a-f]{0,16}$/i', $strReference)) {
             throw new Error('Invalid reference: only alphanumeric chars are allowed, up to 16 chars long');
         }
     }
 
     private static function validatePadChar($strPadChar)
     {
-        if (! preg_match('/^[a-z0-9]{1}$/i', $strPadChar)) {
+        if (!preg_match('/^[a-z0-9]{1}$/i', $strPadChar)) {
             throw new Error('Invalid pad char');
         }
     }
+
+    /**
+     * Get url and qr-image for bancontact
+     * @param $UUID
+     * @param $withBase64 True if you need a base64 image
+     * @return array url, QRUrl and QRBase64
+     */
+    public static function bancontact($UUID, $withBase64 = false)
+    {
+        $qrUrl = 'https://chart.googleapis.com/chart?cht=qr&chs=260x260&chl=https://qr.pisp.me/bc/' . $UUID;
+        $result = [
+            'url' => 'https://qr.pisp.me/bc/' . $UUID,
+            'QRUrl' => $qrUrl
+        ];
+        if($withBase64) $result['QRBase64'] = base64_encode(file_get_contents($qrUrl));
+
+        return $result;
+    }
+
+    /**
+     * Get url and qr-image for ideal
+     *
+     * @param $UUID
+     * @param $withBase64 True if you need a base64 image
+     * @return array url, QRUrl and QRBase64
+     */
+    public static function ideal($UUID, $withBase64 = false)
+    {
+        $qrUrl = 'https://ideal.pay.nl/qr/' . $UUID;
+        $result = [
+            'url' => 'https://qr6.ideal.nl/' . $UUID,
+            'QRUrl' => $qrUrl
+        ];
+
+        if($withBase64) $result['QRBase64'] = base64_encode(file_get_contents($qrUrl));
+
+        return $result;
+    }
+
 
     /**
      * Decode a UUID
@@ -100,7 +139,7 @@ class DynamicUUID
         if (isset($secret)) {
             self::validateSecret($secret);
             $isValid = self::validate($uuid, $secret);
-            if (! $isValid) {
+            if (!$isValid) {
                 throw new Error('Incorrect signature');
             }
         }
@@ -130,12 +169,13 @@ class DynamicUUID
      *
      * @return bool
      */
-    public static function validate($uuid, $secret)
+    public
+    static function validate($uuid, $secret)
     {
         $uuidData = preg_replace('/[^0-9a-z]/i', '', $uuid);
         $uuidData = substr($uuidData, 8);
 
-        $hash     = hash_hmac('sha256', $uuidData, $secret);
+        $hash = hash_hmac('sha256', $uuidData, $secret);
         $checksum = "b" . substr($hash, 0, 7);
 
         return $checksum == substr($uuid, 0, 8);

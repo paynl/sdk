db33f1618dfb038e17dd1adc9551b6fee83e1c26
Jorn Bakker <jorn@pay.nl>
2019-01-21 15:50:50 +0100

    Added asciiToHex for the reference in UUID

--- src/DynamicUUID.php
+++ src/DynamicUUID.php
@@ -21,11 +21,12 @@ class DynamicUUID
      *
      * @return string The UUID
      */
-    public static function encode($serviceId, $secret, $reference, $padChar = '0', $referenceType = self::REFERENCE_TYPE_STRING)
+    public static function encode($serviceId, $secret, $reference, $padChar = '0', $referenceType=self::REFERENCE_TYPE_STRING)
     {
         if ($referenceType == self::REFERENCE_TYPE_STRING) {
             self::validateReferenceString($reference);
-        } else if ($referenceType == self::REFERENCE_TYPE_HEX) {
+            $reference = self::asciiToHex($reference);
+        } else if($referenceType == self::REFERENCE_TYPE_HEX) {
             self::validateReferenceHex($reference);
         }
 
@@ -36,7 +37,7 @@ class DynamicUUID
         $UUIDData = preg_replace('/[^0-9]/', '', $serviceId);
         $UUIDData .= str_pad(strtolower($reference), 16, $padChar, STR_PAD_LEFT);
 
-        $hash = hash_hmac('sha256', $UUIDData, $secret);
+        $hash = hash_hmac(self::HASH_METHOD, $UUIDData, $secret);
 
         $UUID = "b" . substr($hash, 0, 7) . $UUIDData;
 
@@ -48,6 +49,16 @@ class DynamicUUID
             substr($UUID, 16, 4),
             substr($UUID, 20, 12)
         );
+    }
+
+    private static function asciiToHex($ascii) {
+        $hex = '';
+        for ($i = 0; $i < strlen($ascii); $i++) {
+            $byte = strtoupper(dechex(ord($ascii{$i})));
+            $byte = str_repeat('0', 2 - strlen($byte)).$byte;
+            $hex.=$byte."";
+        }
+        return $hex;
     }
 
     private static function validateSecret($strSecret)

1eb27c1d1fe21468f2f8400d5ed854f94f6d14be
Ian Sneep <sneepsken@gmail.com>
2019-08-08 15:42:38 +0200

    Add transaction capture, decline and void (cancel) requests

---
+++ samples/transactions/void.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../init.php';
+
+use PayNL\Sdk\Api;
+use PayNL\Sdk\Request\Transactions\Cancel as VoidTransactionRequest;
+
+$authAdapter = getAuthAdapter();
+
+$transactionId = 'EX-6581-2257-2190';
+$request = new VoidTransactionRequest($transactionId);
+
+$api = new Api($authAdapter);
+$response = $api->handleCall($request);
+
+print '<pre/>';
+print 'transaction ID: ' . $transactionId . PHP_EOL . PHP_EOL;
+print_r($response);
+exit(0);

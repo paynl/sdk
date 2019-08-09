389feb04180ad5af55a7b6869c042fbe8ba9ee7f
Ian Sneep <sneepsken@gmail.com>
2019-08-09 15:28:47 +0200

    Init codeception

---
+++ tests/_support/Helper/Functional.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Helper;
+
+use Codeception\Module;
+
+/**
+ * Class Functional
+ *
+ * here you can define custom actions
+ * all public methods declared in helper class will be available in $I
+ *
+ * @package Helper
+ */
+class Functional extends Module
+{
+}

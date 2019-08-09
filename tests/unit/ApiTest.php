5b5f170bcb2a367d4781a01b4c5c79fdd34f08a2
Ian Sneep <sneepsken@gmail.com>
2019-08-09 16:52:24 +0200

    Add API test class

---
+++ tests/unit/ApiTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\PayNL\Sdk\unit;
+
+use Codeception\Test\Unit as UnitTest;
+
+/**
+ * Class ApiTest
+ *
+ * @package Tests\PayNL\Sdk\unit
+ */
+class ApiTest extends UnitTest
+{
+
+}

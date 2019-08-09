45491fe45d3b59d73a58d342e66de18781f474e3
Ian Sneep <sneepsken@gmail.com>
2019-08-09 15:14:12 +0200

    Introduce AbstractTransformer for shared functionality

---
+++ src/Validator/InputType.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace PayNL\Sdk\Validator;
+
+/**
+ * Class InputType
+ *
+ * @package PayNL\Sdk\Validator
+ */
+class InputType extends AbstractValidator
+{
+    protected const MSG_NO_TYPE            = 'NoType';
+    protected const MSG_VALUE_IS_AN_OBJECT = 'ValueIsAnObject';
+    protected const MSG_WRONG_TYPE         = 'WrongType';
+
+    protected $messageTemplates = [
+        self::MSG_NO_TYPE            => 'No type given to check upon',
+        self::MSG_VALUE_IS_AN_OBJECT => 'Given value is an object',
+        self::MSG_WRONG_TYPE         => '%s is not a(n) %s',
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function isValid($value, string $type = null): bool
+    {
+        if (null === $type) {
+            $this->error(self::MSG_NO_TYPE);
+        }
+
+        if (true === is_object($value)) {
+            $this->error(self::MSG_VALUE_IS_AN_OBJECT);
+        }
+
+        if ($type !== gettype($value) && 0 === count($this->getMessages())) {
+            $this->error(self::MSG_WRONG_TYPE, gettype($value), $type);
+        }
+
+        return 0 === count($this->getMessages());
+    }
+}

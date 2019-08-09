3ce8c0c1d368ba39383e68e52c0f67d700d31aa5
Ian Sneep <sneepsken@gmail.com>
2019-08-09 13:29:44 +0200

    Debug trait for handling debug behavior

---
+++ src/DebugTrait.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace PayNL\Sdk;
+
+/**
+ * Trait DebugTrait
+ *
+ * @package PayNL\Sdk
+ */
+trait DebugTrait
+{
+    /**
+     * @var bool
+     */
+    protected $debug = false;
+
+    /**
+     * @return bool
+     */
+    public function getDebug(): bool
+    {
+        return $this->debug;
+    }
+
+    /**
+     * @param bool $debug
+     *
+     * @return self
+     */
+    public function setDebug(bool $debug): self
+    {
+        $this->debug = $debug;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDebug(): bool
+    {
+        return true === $this->getDebug();
+    }
+
+    /**
+     * Dumps the given arguments
+     *
+     * @param array $arguments
+     *
+     * @return void
+     */
+    public function dumpDebugInfo(...$arguments): void
+    {
+        $function = 'var_dump';
+        $dumpString = '<pre>%s</pre>' . PHP_EOL;
+        if (true === function_exists('dump')) {
+            $function = 'dump';
+            $dumpString = '';
+        }
+
+        echo sprintf(
+            $dumpString,
+            $function(...$arguments)
+        );
+    }
+}

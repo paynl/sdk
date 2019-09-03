7df6bd35f6ccbbc548aa2f06d13663e1e758f112
Ian Sneep <sneepsken@gmail.com>
2019-09-03 10:38:37 +0200

    PAY-65: Use ServiceIdTrait instead of inherit Get request

--- src/Request/Services/GetCategories.php
+++ src/Request/Services/GetCategories.php
@@ -3,18 +3,41 @@ declare(strict_types=1);
 
 namespace PayNL\Sdk\Request\Services;
 
+use PayNL\Sdk\Request\AbstractRequest;
+use PayNL\Sdk\Request\Parameter\ServiceIdTrait;
+
 /**
  * Class GetCategories
  *
  * @package PayNL\Sdk\Request\Services
  */
-class GetCategories extends Get
+class GetCategories extends AbstractRequest
 {
+    use ServiceIdTrait;
+
+    /**
+     * GetCategories constructor.
+     *
+     * @param string $serviceId
+     */
+    public function __construct(string $serviceId)
+    {
+        $this->setServiceId($serviceId);
+    }
+
     /**
      * @inheritDoc
      */
     public function getUri(): string
     {
         return "services/{$this->getServiceId()}/categories";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMethod(): string
+    {
+        return static::METHOD_GET;
     }
 }
--- src/Request/Services/GetPaymentMethods.php
+++ src/Request/Services/GetPaymentMethods.php
@@ -3,15 +3,41 @@ declare(strict_types=1);
 
 namespace PayNL\Sdk\Request\Services;
 
+use PayNL\Sdk\Request\AbstractRequest;
+use PayNL\Sdk\Request\Parameter\ServiceIdTrait;
+
 /**
  * Class GetPaymentMethods
  *
  * @package PayNL\Sdk\Request\Services
  */
-class GetPaymentMethods extends Get
+class GetPaymentMethods extends AbstractRequest
 {
+    use ServiceIdTrait;
+
+    /**
+     * GetPaymentMethods constructor.
+     *
+     * @param string $serviceId
+     */
+    public function __construct(string $serviceId)
+    {
+        $this->setServiceId($serviceId);
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function getUri(): string
     {
         return "services/{$this->getServiceId()}/paymentmethods";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMethod(): string
+    {
+        return static::METHOD_GET;
     }
 }

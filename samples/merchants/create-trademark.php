fa5b56823c5c3bc412e0a120b539b1e0e02599c3
Ian Sneep <sneepsken@gmail.com>
2019-09-04 09:22:59 +0200

    PAY-65: Create trademark stub

--- samples/merchants/create-trademark.php
+++ samples/merchants/create-trademark.php
@@ -1,2 +1,18 @@
 <?php
+declare(strict_types=1);
+throw new Exception('TODO: FIX REST API');
+require_once __DIR__ . '/../init.php';
 
+use PayNL\Sdk\Api;
+use PayNL\Sdk\Request\Merchants\AddTrademark as AddTrademarkRequest;
+
+$authAdapter = getAuthAdapter();
+
+$request = new AddTrademarkRequest();
+
+$api = new Api($authAdapter);
+$response = $api->handleCall($request);
+
+print '<pre/>';
+print_r($response);
+exit(0);

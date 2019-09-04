26895bd9f8e1cf0a628cb105c266ae4d576b9f60
Ian Sneep <sneepsken@gmail.com>
2019-09-04 08:19:59 +0200

    PAY-65: Get merchant request

---
+++ src/Request/Parameter/MerchantIdTrait.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace PayNL\Sdk\Request\Parameter;
+
+/**
+ * Trait MerchantIdTrait
+ *
+ * @package PayNL\Sdk\Request\Parameter
+ */
+trait MerchantIdTrait
+{
+    /**
+     * @var string
+     */
+    protected $merchantId;
+
+    /**
+     * @return string
+     */
+    public function getMerchantId(): string
+    {
+        return $this->merchantId;
+    }
+
+    /**
+     * @param string $merchantId
+     *
+     * @return MerchantIdTrait
+     */
+    public function setMerchantId(string $merchantId): self
+    {
+        $this->merchantId = $merchantId;
+        return $this;
+    }
+}

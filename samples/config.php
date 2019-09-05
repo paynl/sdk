e19659ada176026afdbd4a15206d0091b686b4fd
Ian Sneep <sneepsken@gmail.com>
2019-09-05 08:31:12 +0200

    PAY-65: Make API URL configurable + Introduce Config object

--- samples/config.php
+++ samples/config.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 return [
+    'api_url'  => 'https://rest.idefix-rest-api-mike.ian.dev.pay.nl/v1/',
     'username' => 'token',
     'password' => '68babb1a525f6116b387231af9d2e4413a6c8f61',
 //    'password' => 'd25895ff665de1a2afd67e264a136d16189b1d45',

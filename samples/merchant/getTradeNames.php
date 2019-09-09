8879d312565c6a4af311924d081b4282afefd3e0
Ian Sneep <sneepsken@gmail.com>
2019-09-09 14:08:01 +0200

    Clear structure

--- samples/merchant/getTradeNames.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once '../../vendor/autoload.php';
-require_once '../config.php';
-
-$tradeNames = \Paynl\Merchant::getTradeNames('M-3421-2120');
--- samples/merchant/info.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once '../../vendor/autoload.php';
-require_once '../config.php';
-
-$result = \Paynl\Merchant::info('M-1234-5678');

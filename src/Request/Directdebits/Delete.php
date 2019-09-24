1666278851d1b2994ae3159a3087de0cf610b92b
Ian Sneep <sneepsken@gmail.com>
2019-09-24 09:09:24 +0200

    PAY-65: Add directdebit delete request

--- src/Request/Directdebits/Delete.php
+++ src/Request/Directdebits/Delete.php
@@ -1,10 +1,44 @@
 <?php
 
+declare(strict_types=1);
 
 namespace PayNL\Sdk\Request\Directdebits;
 
+use PayNL\Sdk\Request\AbstractRequest;
+use PayNL\Sdk\Request\Parameter\IncassoOrderIdTrait;
 
-class Delete
+/**
+ * Class Delete
+ *
+ * @package PayNL\Sdk\Request\Directdebits
+ */
+class Delete extends AbstractRequest
 {
+    use IncassoOrderIdTrait;
 
+    /**
+     * Delete constructor.
+     *
+     * @param string $incassoOrderId
+     */
+    public function __construct(string $incassoOrderId)
+    {
+        $this->setIncassoOrderId($incassoOrderId);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUri(): string
+    {
+        return "directdebits/{$this->getIncassoOrderId()}";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMethod(): string
+    {
+        return static::METHOD_DELETE;
+    }
 }

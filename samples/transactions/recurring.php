9f35c6a83871fb1cbc43a820d8f19a19e269d138
Ian Sneep <sneepsken@gmail.com>
2019-10-01 11:32:18 +0200

    PAY-65: Adjust the recurring transaction sample so it uses the config instance to get the transaction id

--- samples/transactions/recurring.php
+++ samples/transactions/recurring.php
@@ -4,30 +4,35 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../init.php';
 
-use PayNL\Sdk\Api;
+use PayNL\Sdk\{
+    Api,
+    Config
+};
 use PayNL\Sdk\Request\Transactions\Recurring as RecurringTransactionRequest;
 use PayNL\Sdk\Hydrator\RecurringTransaction as RecurringTransactionHydrator;
 use PayNL\Sdk\Model\RecurringTransaction;
 
 $authAdapter = getAuthAdapter();
 
-$transactionId = 'EX-6581-2257-2190';
-$request = new RecurringTransactionRequest($transactionId, (new RecurringTransactionHydrator())->hydrate([
-    'amount' => [
-        'amount' => 10,
-        'currency' => 'EUR'
-    ],
-    'description' => 'Test recurring',
-    'extra1' => 'Extra 1',
-    'extra2' => 'Extra 2',
-    'extra3' => 'Extra 3',
-], new RecurringTransaction()));
+$request = (new RecurringTransactionRequest(
+    Config::getInstance()->get('transactionId'),
+    (new RecurringTransactionHydrator())->hydrate([
+        'amount' => [
+            'amount' => 10,
+            'currency' => 'EUR'
+        ],
+        'description' => 'Test recurring',
+        'extra1' => 'Extra 1',
+        'extra2' => 'Extra 2',
+        'extra3' => 'Extra 3',
+    ], new RecurringTransaction())
+))
+    ->setDebug(true);
 
 $response = (new Api($authAdapter))
-    ->setDebug(true)
     ->handleCall($request)
 ;
 
-echo '<pre/>';
-print_r($response);
-exit(0);
+echo '<pre/>' . PHP_EOL .
+    var_export($response, true)
+;

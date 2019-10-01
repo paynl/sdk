9834a515871fe9becba5b78ff835ed3b9770cb68
Ian Sneep <sneepsken@gmail.com>
2019-10-01 16:46:40 +0200

    PAY-65: Make index.php dynamic and based on the contents of the samples folder

--- samples/index.php
+++ samples/index.php
@@ -7,6 +7,9 @@ declare(strict_types=1);
     <head>
         <title>Samples</title>
         <style>
+            h2 {
+                float: left;
+            }
             div {
                 display: block;
                 position: relative;
@@ -22,119 +25,41 @@ declare(strict_types=1);
     <body>
         <div id="main">
             <h1>Samples</h1>
-            <h2>Currencies</h2>
-            <div>
-                <a href="currencies/get-all.php" target="_blank">Get all currencies</a>
-            </div>
-            <div>
-                <a href="currencies/get.php" target="_blank">Get currency</a>
-            </div>
-            <h2>DirectDebits</h2>
-            <div>
-                <a href="directdebits/get.php" target="_blank">Get directdebit order</a>
-            </div>
-            <div>
-                <a href="directdebits/create.php" target="_blank">Create new directdebit order</a>
-            </div>
-            <div>
-                <a href="directdebits/update.php" target="_blank">Update directdebit order</a>
-            </div>
-            <div>
-                <a href="directdebits/remove.php" target="_blank">Remove directdebit order</a>
-            </div>
-            <div>
-                <a href="directdebits/create-recurring.php" target="_blank">Create a recurring debit</a>
-            </div>
-            <h2>IsPay</h2>
-            <div>
-                <a href="is-pay/check-ip.php" target="_blank">Check IP</a>
-            </div>
-            <h2>Merchants</h2>
-            <div>
-                <a href="merchants/get.php" target="_blank">Get a merchant</a>
-            </div>
-            <div>
-                <a href="merchants/create-trademark.php" target="_blank">Create a trademark</a>
-            </div>
-            <div>
-                <a href="merchants/remove-trademark.php" target="_blank">Remove trademark</a>
-            </div>
-            <h2>Pin</h2>
-            <div>
-                <a href="pin/get-all-terminals.php" target="_blank">Get all pin terminals</a>
-            </div>
-            <div>
-                <a href="pin/pay.php" target="_blank">Pay pin transaction</a>
-            </div>
-            <div>
-                <a href="pin/get-terminal-transaction-status.php" target="_blank">Get terminal transaction status</a>
-            </div>
-            <div>
-                <a href="pin/confirm-terminal-transaction.php" target="_blank">Confirm terminal transaction</a>
-            </div>
-            <div>
-                <a href="pin/get-receipt.php" target="_blank">Get pin receipt</a>
-            </div>
-            <h2>Refunds</h2>
-            <div>
-                <a href="refunds/get.php" target="_blank">Get Refund</a>
-            </div>
-            <h2>Services</h2>
-            <div>
-                <a href="services/get-all.php" target="_blank">Get all services</a>
-            </div>
-            <div>
-                <a href="services/get.php" target="_blank">Get a service</a>
-            </div>
-            <div>
-                <a href="services/get-paymethods.php" target="_blank">Get service payment methods</a>
-            </div>
-            <div>
-                <a href="services/create-payment-link.php" target="_blank">Create service payment link</a>
-            </div>
-            <h2>Transactions</h2>
-            <div>
-                <a href="transactions/get.php" target="_blank">Get a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/create.php" target="_blank">Create a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/approve.php" target="_blank">Approve a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/capture.php" target="_blank">Capture a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/decline.php" target="_blank">Decline a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/void.php" target="_blank">Cancel a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/refund.php" target="_blank">Refund a Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/recurring.php" target="_blank">Add a recurring Transaction</a>
-            </div>
-            <div>
-                <a href="transactions/qr.php" target="_blank">Capture Transaction by QR code</a>
-            </div>
-            <div>
-                <a href="transactions/tokenize.php" target="_blank">Tokenize Transaction</a>
-            </div>
-            <h2>Voucher</h2>
-            <div>
-                <a href="voucher/activate.php" target="_blank">Activate voucher</a>
-            </div>
-            <div>
-                <a href="voucher/balance.php" target="_blank">Get voucher balance</a>
-            </div>
-            <div>
-                <a href="voucher/charge.php" target="_blank">Charge voucher</a>
-            </div>
+            <?php
+
+            $entries = glob(__DIR__ . DIRECTORY_SEPARATOR . '*');
+            foreach ($entries as $entry) {
+                if (true === is_file($entry)) {
+                    continue;
+                }
+
+                $samples = glob($entry . DIRECTORY_SEPARATOR . '*.php');
+                if (0 === count($samples)) {
+                    continue;
+                }
+
+                echo sprintf(
+                    '<h2>%s</h2>' . PHP_EOL,
+                    str_replace(' ', '', ucwords(str_replace('-', ' ', basename($entry))))
+                );
+
+                foreach ($samples as $sample) {
+                    echo sprintf(
+                        '<div>
+                            <a href="%s" target="_blank">%s</a>
+                        </div>' . PHP_EOL,
+                        ltrim(str_replace(__DIR__, '', $sample), DIRECTORY_SEPARATOR),
+                        str_replace(['-', '.php'], [' ', ''], ucfirst(basename($sample)))
+                    );
+                }
+
+            }
+
+            ?>
             <h2>QR codes</h2>
-            <p>TODO?</p>
+            <div>
+                <p>TODO?</p>
+            </div>
         </div>
     </body>
 </html>

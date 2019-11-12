eb9354f761d72c88dbde755ec30c4e2bf32a78be
daan <daan@pay.nl>
2019-11-12 11:42:43 +0100

    Cleans feature/v2 branch.

--- samples/directDebit/get.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../vendor/autoload.php';
-require_once '../config.php';
-try {
-    $result = \Paynl\DirectDebit::get('IO-9350-3111-3010');
-    var_dump($result->getData());
-} catch (\Paynl\Error\Error $e) {
-    echo "Error: ".$e->getMessage();
-}
--- samples/merchant/deleteTradeName.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../vendor/autoload.php';
-require_once '../config.php';
-
-$result = \Paynl\Merchant::deleteTradeName('TM-3311-7380');
-
-var_dump($result);

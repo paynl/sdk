932242568ef143dc3fd854fd807ae01d45ae692d
Ian Sneep <sneepsken@gmail.com>
2020-01-13 16:27:24 +0100

    PAY-65: Fix GetReceipt request + Set Errors model from transformer

--- samples/pin/get-receipt.php
+++ samples/pin/get-receipt.php
@@ -5,20 +5,37 @@ declare(strict_types=1);
 require_once __DIR__ . '/../init.php';
 
 use PayNL\Sdk\{
-    Api,
-    Config
+    Application\Application,
+    Config\Config
 };
-use PayNL\Sdk\Request\Pin\GetReceipt as GetPinReceiptRequest;
 
-$authAdapter = getAuthAdapter();
-$request = (new GetPinReceiptRequest(Config::getInstance()->get('terminalTransactionId')))
-    ->setDebug((bool)Config::getInstance()->get('debug'))
+$response = Application::init(Config::getInstance()->toArray())
+    ->setRequest('GetReceipt', [
+        'terminalTransactionId' => Config::getInstance()->get('terminalTransactionId'),
+    ])
+    ->run()
 ;
 
-$response = (new Api($authAdapter))
-    ->handleCall($request)
-;
-
+// NOTE: only approved trademarks are given to the response merchant object, the new trademark isn't instantly approved
 echo '<pre/>' . PHP_EOL .
-    var_export($response, true)
+    var_export($response->getBody(), true)
 ;
+
+//use PayNL\Sdk\{
+//    Api,
+//    Config
+//};
+//use PayNL\Sdk\Request\Pin\GetReceipt as GetPinReceiptRequest;
+//
+//$authAdapter = getAuthAdapter();
+//$request = (new GetPinReceiptRequest(Config::getInstance()->get('terminalTransactionId')))
+//    ->setDebug((bool)Config::getInstance()->get('debug'))
+//;
+//
+//$response = (new Api($authAdapter))
+//    ->handleCall($request)
+//;
+//
+//echo '<pre/>' . PHP_EOL .
+//    var_export($response, true)
+//;

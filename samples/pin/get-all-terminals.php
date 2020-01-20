c25ff7d9b6b8c34881f93f0b339ac143be5c4c43
Ian Sneep <sneepsken@gmail.com>
2020-01-20 10:03:06 +0100

    PAY-65: Fix pin samples

--- samples/pin/get-all-terminals.php
+++ samples/pin/get-all-terminals.php
@@ -10,18 +10,3 @@ $response = $app
 ;
 
 print_response($response);
-
-/*use PayNL\Sdk\{
-    Application\Application,
-    Config\Config
-};
-
-$response = Application::init(Config::getInstance()->toArray())
-    ->setRequest('GetTerminals')
-    ->run()
-;
-
-// NOTE: only approved trademarks are given to the response merchant object, the new trademark isn't instantly approved
-echo '<pre/>' . PHP_EOL .
-    var_export($response->getBody(), true)
-;*/
--- samples/pin/make-payment.php
+++ samples/pin/make-payment.php
@@ -2,28 +2,21 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../init_application.php';
+$app = require __DIR__ . '/../init_application.php';
 
-use PayNL\Sdk\{
-    Api,
-    Config
-};
-use PayNL\Sdk\Request\Pin\PayTransaction as PayTransactionRequest;
-use PayNL\Sdk\Hydrator\Simple as SimpleHydrator;
-use PayNL\Sdk\Model\Terminal;
-
-$authAdapter = getAuthAdapter();
-
-$request = (new PayTransactionRequest(Config::getInstance()->get('transactionId'), (new SimpleHydrator())->hydrate([
-    'id' => Config::getInstance()->get('terminalId'),
-], new Terminal())))
-    ->setDebug((bool)Config::getInstance()->get('debug'))
+$response = $app
+    ->setRequest(
+        'PayTransaction',
+        [
+            'transactionId' => $config->get('transactionId'),
+        ],
+        [
+            'Terminal' => [
+                'id' => $config->get('terminalId'),
+            ],
+        ]
+    )
+    ->run()
 ;
 
-$response = (new Api($authAdapter))
-    ->handleCall($request)
-;
-
-echo '<pre/>' . PHP_EOL .
-    var_export($response, true)
-;
+print_response($response);

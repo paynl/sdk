e63864232ab198b8bd598efc3f994b5488616bc8
Ian Sneep <sneepsken@gmail.com>
2020-01-20 12:24:23 +0100

    PAY-65: Remove commented code

--- samples/refunds/get-a-refund.php
+++ samples/refunds/get-a-refund.php
@@ -12,25 +12,3 @@ $response = $app
 ;
 
 print_response($response);
-
-/*require_once __DIR__ . '/../init_application.php';
-
-use PayNL\Sdk\{
-    Api,
-    Config
-};
-use PayNL\Sdk\Request\Refunds\Get as GetRefundRequest;
-
-$authAdapter = getAuthAdapter();
-
-$request = (new GetRefundRequest('RF-7039-3062-3700'))
-    ->setDebug((bool)Config::getInstance()->get('debug'))
-;
-
-$response = (new Api($authAdapter))
-    ->handleCall($request)
-;
-
-echo '<pre/>' . PHP_EOL .
-    var_export($response, true)
-;*/

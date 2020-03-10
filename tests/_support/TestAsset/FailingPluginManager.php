1209e7b4f234db09f875a17863103839b6dcac2b
Ian Sneep <sneepsken@gmail.com>
2020-03-10 16:17:15 +0100

    PAY-65: Add TestAssets for unit testing + Add config path to include the TestAsset Manager (SimpleManager) to "autoload"

---
+++ tests/_support/TestAsset/FailingPluginManager.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codeception\TestAsset;
+
+use PayNL\Sdk\Service\Manager;
+
+/**
+ * Class FailingPluginManager
+ *
+ * @package Codeception\TestAsset
+ */
+class FailingPluginManager extends Manager
+{
+    protected $instanceOf = InvokableObject::class;
+}

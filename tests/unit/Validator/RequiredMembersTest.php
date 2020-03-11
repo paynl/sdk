e08c948c7d12f4ea2ccc6e457fb854c59ffc42ad
Ivo Wams <ivo@pay.nl>
2020-03-11 09:01:04 +0100

    Finalize RequiredMembersTest

--- tests/unit/Validator/RequiredMembersTest.php
+++ tests/unit/Validator/RequiredMembersTest.php
@@ -80,14 +80,15 @@ class RequiredMembersTest extends UnitTest
 
     public function testItCanGetDataFromAGivenObject(): void
     {
+        $this->setHydrator();
         $this->dummy->setRequiredMember('some-string');
 
         $data = $this->tester->invokeMethod($this->validator, 'getDataFromObject', [$this->dummy]);
         verify($data)->array();
         verify($data)->notEmpty();
-        verify($data)->hasKey('required_member');
-        verify($data['required_member'])->string();
-        verify($data['required_member'])->equals('some-string');
+        verify($data)->hasKey('requiredMember');
+        verify($data['requiredMember'])->string();
+        verify($data['requiredMember'])->equals('some-string');
     }
 
     public function testItCanCheckForRequiredMembers(): void
@@ -129,6 +130,44 @@ class RequiredMembersTest extends UnitTest
         verify($result)->false();
     }
 
+    public function testItCanTestEmptyOnEmptyOrNullString(): void
+    {
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['', '']);
+        verify($data)->bool();
+        verify($data)->true();
+
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['', null]);
+        verify($data)->bool();
+        verify($data)->true();
+
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['id', 0]);
+        verify($data)->bool();
+        verify($data)->true();
+
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['transactionId', 0]);
+        verify($data)->bool();
+        verify($data)->true();
+    }
+
+    public function testItCanTestOnNotEmpty(): void
+    {
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['', '12345']);
+        verify($data)->bool();
+        verify($data)->false();
+
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['id', '12345']);
+        verify($data)->bool();
+        verify($data)->false();
+
+        $data = $this->tester->invokeMethod($this->validator, 'isEmpty', ['getId', '12345']);
+        verify($data)->bool();
+        verify($data)->false();
+    }
+
+    /**
+     * @depends testItCanTestEmptyOnEmptyOrNullString
+     * @depends testItCanTestOnNotEmpty
+     */
     public function testValidateCorrectly(): void
     {
         $this->setHydrator();

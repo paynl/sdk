32e49333d2d61187e9c93e82e880d48eeff4f2f6
Kevin Verschoor <k.verschoor@pay.nl>
2020-03-18 12:06:16 +0100

    Fix new version of refund to work with getRefundId(), Add more getters for all data, Update the sample of Refund

--- src/Result/Transaction/Refund.php
+++ src/Result/Transaction/Refund.php
@@ -10,12 +10,70 @@ use Paynl\Result\Result;
  * @author Andy Pieters <andy@pay.nl>
  */
 class Refund extends Result
-{
+{   
+
+    /**
+     * @return string The description
+     */
+    public function getDescription()
+    {            
+        return (isset($this->data['description'])) ? $this->data['description'] : '';       
+    }   
+
+    /**
+     * @return string The Refund
+     */
+    public function getRefundedTransaction()
+    {        
+        $refundedTransaction = array();          
+        if(isset($this->data['refundedTransactions'])){
+            $refundedTransaction = reset($this->data['refundedTransactions']);
+        }    
+        return $refundedTransaction;       
+    }   
+    
+    /**
+     * @return string The orderId
+     */
+    public function getOrderId()
+    {     
+        $refundedTransaction = $this->getRefundedTransaction();
+        return (isset($refundedTransaction['orderId'])) ? $refundedTransaction['orderId'] : '';       
+    }
+    
+    /**
+     * @return string The total amount paid for the transaction
+     */
+    public function getAmount()
+    {     
+        $refundedTransaction = $this->getRefundedTransaction();
+        return (isset($refundedTransaction['amount'])) ? $refundedTransaction['amount'] : '';       
+    }
+    
+    /**
+     * @return string The refundAmount
+     */
+    public function getRefundAmount()
+    {     
+        $refundedTransaction = $this->getRefundedTransaction();
+        return (isset($refundedTransaction['refundAmount'])) ? $refundedTransaction['refundAmount'] : '';       
+    }
+
+    /**
+     * @return string The bankaccountNumber
+     */
+    public function getBankaccountNumber()
+    {     
+        $refundedTransaction = $this->getRefundedTransaction();
+        return (isset($refundedTransaction['bankaccountNumber'])) ? $refundedTransaction['bankaccountNumber'] : '';       
+    }
+
     /**
      * @return string The refundId
      */
     public function getRefundId()
-    {
-        return $this->data['refundId'];
+    {     
+        $refundedTransaction = $this->getRefundedTransaction();
+        return (isset($refundedTransaction['refundId'])) ? $refundedTransaction['refundId'] : '';       
     }
 }

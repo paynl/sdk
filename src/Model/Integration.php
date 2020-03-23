0c1629c50d90675c4471c5ab257bb79486dff78d
Ian Sneep <sneepsken@gmail.com>
2020-03-23 16:57:31 +0100

    PAY-65: Adjust the integration model, because test mode is now a boolean its no longer needed to check the given value to setTestMode. Because of this remove the getTestModes method + Add IntegrationTest

--- src/Model/Integration.php
+++ src/Model/Integration.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace PayNL\Sdk\Model;
 
 use JsonSerializable;
-use PayNL\Sdk\{
-    Exception\InvalidArgumentException,
-    Common\JsonSerializeTrait
-};
+use PayNL\Sdk\Common\JsonSerializeTrait;
 
 /**
  * Class Integration
@@ -33,17 +30,6 @@ class Integration implements ModelInterface, JsonSerializable
     protected $testMode = self::TEST_MODE_OFF;
 
     /**
-     * @return array
-     */
-    protected function getTestModes(): array
-    {
-        return [
-            static::TEST_MODE_OFF,
-            static::TEST_MODE_ON,
-        ];
-    }
-
-    /**
      * @return boolean
      */
     public function isTestMode(): bool
@@ -54,23 +40,10 @@ class Integration implements ModelInterface, JsonSerializable
     /**
      * @param boolean $testMode
      *
-     * @throws InvalidArgumentException when given test mode is invalid
-     *
      * @return Integration
      */
     public function setTestMode(bool $testMode): self
     {
-        $allowedModes = $this->getTestModes();
-        if (false === in_array($testMode, $allowedModes, true)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'Given test mode "%d" is not allowed, choose one of: "%s"',
-                    $testMode,
-                    implode('", "', $allowedModes)
-                )
-            );
-        }
-
         $this->testMode = $testMode;
         return $this;
     }

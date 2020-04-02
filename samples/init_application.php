536a1a0a0e124e240e793c3b39f3ff1af559ff5d
Ian Sneep <sneepsken@gmail.com>
2020-04-02 16:28:40 +0200

    PAY-65: Put print_response function in its own file and autoload it with composer

--- samples/init_application.php
+++ samples/init_application.php
@@ -6,26 +6,8 @@ require_once  __DIR__ . '/../vendor/autoload.php';
 
 use PayNL\Sdk\{
     Application\Application,
-    Config\Config,
-    Response\ResponseInterface
+    Config\Config
 };
-
-if (false === function_exists('print_response')) {
-    /**
-     * Function to quickly "dump" the response given by exporting
-     * the body to a string
-     *
-     * @param ResponseInterface $response
-     *
-     * @return void
-     */
-    function print_response(ResponseInterface $response) {
-        echo '<pre>' . PHP_EOL .
-            var_export($response->getBody(), true) . PHP_EOL .
-            '</pre>' . PHP_EOL
-        ;
-    }
-}
 
 $config = new Config(require 'config.php');
 
---
+++ samples/print_response.function.php
@@ -0,0 +1,20 @@
+<?php
+
+use PayNL\Sdk\Response\ResponseInterface;
+
+if (false === function_exists('print_response')) {
+    /**
+     * Function to quickly "dump" the response given by exporting
+     * the body to a string
+     *
+     * @param ResponseInterface $response
+     *
+     * @return void
+     */
+    function print_response(ResponseInterface $response) {
+        echo '<pre>' . PHP_EOL .
+            var_export($response->getBody(), true) . PHP_EOL .
+            '</pre>' . PHP_EOL
+        ;
+    }
+}

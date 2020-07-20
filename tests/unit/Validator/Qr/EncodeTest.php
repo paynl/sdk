db8718350a6fe9b5c1f42570014ec3882d6dc34f
cryptokittycat <daan@pay.nl>
2020-07-20 11:11:48 +0200

    Add unit tests for new function/classes

---
+++ tests/unit/Validator/Qr/EncodeTest.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PayNL\Sdk\Validator\Qr;
+
+use CodeCeption\Test\Unit as UnitTest;
+use Codeception\TestAsset\DummyQr;
+use PayNL\Sdk\Validator\AbstractValidator;
+use PayNL\Sdk\Validator\Qr\Encode;
+use PayNL\Sdk\Validator\ValidatorInterface;
+use UnitTester;
+use Zend\Hydrator\ClassMethods;
+
+/**
+ * Class EncodeTest
+ *
+ * @package Tests\Unit\PayNL\Sdk\Validator
+ */
+class EncodeTest extends UnitTest
+{
+    /** @var Encode */
+    protected $validator;
+
+    /** @var DummyQr */
+    private $dummy;
+
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @returns void
+     */
+    protected function _before(): void
+    {
+        $this->validator = new Encode();
+
+        $this->dummy = new DummyQr();
+    }
+
+    /**
+     * @returns void
+     */
+    private function setHydrator(): void
+    {
+        $this->validator->setHydrator(new ClassMethods(false, true));
+    }
+
+    /**
+     * @return void
+     */
+    public function testItIsAValidator(): void
+    {
+        verify($this->validator)->isInstanceOf(ValidatorInterface::class);
+    }
+
+    /**
+     * @return void
+     */
+    public function testItExtendsAbstract(): void
+    {
+        verify($this->validator)->isInstanceOf(AbstractValidator::class);
+    }
+
+    /**
+     * @return void
+     */
+    public function testItCanValidate(): void
+    {
+        $this->setHydrator();
+
+        $this->tester->assertObjectHasMethod('isValid', $this->validator);
+        $this->tester->assertObjectMethodIsPublic('isValid', $this->validator);
+        $data = $this->tester->invokeMethod($this->validator, 'isValid', [$this->dummy]);
+        $this->assertIsBool($data);
+        $this->assertFalse($data);
+        $this->assertNotEmpty($this->validator->getMessages());
+    }
+
+    /**
+     * @return void
+     */
+    public function testItCanValidateWithEmptyMembers(): void
+    {
+        $this->setHydrator();
+
+        $this->dummy->setSecret('');
+        $data = $this->validator->isValid($this->dummy);
+        $this->assertIsBool($data);
+        $this->assertFalse($data);
+        $this->assertNotEmpty($this->validator->getMessages());
+    }
+
+    /**
+     * @return void
+     */
+    public function testItCanValidateWithMissingMembers(): void
+    {
+        $this->setHydrator();
+
+        $data = $this->validator->isValid($this->dummy);
+        $this->assertIsBool($data);
+        $this->assertFalse($data);
+        $this->assertNotEmpty($this->validator->getMessages());
+    }
+
+    /**
+     * @returns void
+     */
+    public function testItCanValidateCorrectly(): void
+    {
+        $this->setHydrator();
+
+        $this->dummy->setSecret('secret');
+        $data = $this->validator->isValid($this->dummy);
+        $this->assertIsBool($data);
+        $this->assertTrue($data);
+        $this->assertEmpty($this->validator->getMessages());
+    }
+}

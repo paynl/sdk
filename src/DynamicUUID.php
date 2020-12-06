785fa5b47d0c74e05409856ad198406149f95938
Manuel de Ruiter <manuel@inquiry.center>
2020-12-07 00:40:39 +0100

    Curly braces for offset access has been deprecated in PHP 7.4 and is removed in PHP 8.

--- src/DynamicUUID.php
+++ src/DynamicUUID.php
@@ -55,7 +55,7 @@ class DynamicUUID
     private static function asciiToHex($ascii) {
         $hex = '';
         for ($i = 0; $i < strlen($ascii); $i++) {
-            $byte = strtoupper(dechex(ord($ascii{$i})));
+            $byte = strtoupper(dechex(ord($ascii[$i])));
             $byte = str_repeat('0', 2 - strlen($byte)).$byte;
             $hex.=$byte."";
         }

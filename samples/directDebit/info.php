c2ede866df1243e4d124ada20085cd3d1bea37ad
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-11-10 15:02:39 +0100

    Update Samples

--- samples/directDebit/info.php
+++ samples/directDebit/info.php
@@ -2,7 +2,7 @@
 require_once '../../vendor/autoload.php';
 require_once '../config.php';
 try {
-    $result = \Paynl\DirectDebit::info('IO-9350-3111-3010' /*Required*/, 'IL-1643-7168-1810' /*Optional*/);
+    $result = \Paynl\DirectDebit::info('IO-1234-1234-1234' /*Required*/, 'IL-1234-1234-1234' /*Optional*/);
     var_dump($result->getData());
 } catch (\Paynl\Error\Error $e) {
     echo "Error: " . $e->getMessage();
